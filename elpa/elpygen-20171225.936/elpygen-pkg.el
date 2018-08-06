912bb25e4c4d4bcdfbeb219d7734bef081ec3112
Shyam <shyam.perumbil@gmail.com>
2018-08-06 21:17:31 +0530

    first

---
+++ elpa/elpygen-20171225.936/elpygen-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "elpygen" "20171225.936" "Generate a Python function/method using a symbol under point" '((emacs "25") (yasnippet "0.8.0")))
---
+++ init.el
@@ -0,0 +1,56 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;;(require 'package)
+;;(add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/") t)
+;;(package-initialize)
+ (require 'package)
+(add-to-list 'package-archives
+              '("melpa-stable" . "https://stable.melpa.org/packages/"))
+ (package-initialize)
+ (setq package-archives
+       '(("GNU ELPA"     . "http://elpa.gnu.org/packages/")
+         ("MELPA Stable" . "https://stable.melpa.org/packages/")
+         ("MELPA"        . "https://melpa.org/packages/"))
+       package-archive-priorities
+       '(("MELPA Stable" . 1)
+         ("GNU ELPA"     . 3)
+         ("MELPA"        . 2)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes nil)
+ '(package-selected-packages
+   (quote
+    (elpygen evil pyvenv elpy magit emmet-mode web-mode jinja2-mode markdown-mode racket-mode ## sml-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(comint-highlight-prompt ((t (:inherit minibuffer-prompt :foreground "brightred"))))
+ '(custom-comment-tag ((t (:foreground "cyan"))))
+ '(custom-face-tag ((t (:foreground "brightblack"))))
+ '(dired-directory ((t (:foreground "white"))))
+ '(evil-ex-lazy-highlight ((t (:background "black" :foreground "brightwhite"))))
+ '(font-lock-comment-delimiter-face ((t (:inherit default))))
+ '(font-lock-comment-face ((t (:foreground "brightblue"))))
+ '(font-lock-function-name-face ((t (:foreground "brightred"))))
+ '(font-lock-keyword-face ((t (:foreground "brightmagenta"))))
+ '(font-lock-string-face ((t (:foreground "brightgreen"))))
+ '(highlight-indentation-face ((t (:background "brightblue" :foreground "brightblack"))))
+ '(isearch-fail ((t (:background "white"))))
+ '(markdown-markup-face ((t (:slant normal :weight normal))))
+ '(minibuffer-prompt ((t (:foreground "brightblue"))))
+ '(racket-keyword-argument-face ((t (:foreground "brightyellow"))))
+ '(racket-logger-config-face ((t (:inherit font-lock-comment-face :foreground "brightmagenta"))))
+ '(region ((t (:background "lightgoldenrod2" :foreground "brightred" :underline nil)))))
+(desktop-save-mode 1)
+(require 'evil)
+(evil-mode 1)
+(package-initialize)
+(elpy-enable)
